9026bb4dfa51caac0e55c645b8242b0c3d102e8a
qyzhencharles <qyzhencharles@gmail.com>
2023-10-24 17:06:21 -0400

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id: "1",
+        name: "Peter",
+        age: "30"
+    },
+    {
+        id: "2",
+        name: "Mark",
+        age: "20"
+    },
+    {
+        id: "3",
+        name: "Sam",
+        age: "20"
+    }
+]
+
+export default Employees;
